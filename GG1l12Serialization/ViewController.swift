b98da24d4b724bcbe9d15b47196e0d0fd93fac5a
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-06-02 19:54:06 +0300

    Initial Commit

---
+++ GG1l12Serialization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GG1l12Serialization
+//
+//  Created by Ivan Vasilevich on 6/2/17.
+//  Copyright © 2017 Smoosh Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
